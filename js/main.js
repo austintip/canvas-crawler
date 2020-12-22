ca87f3e704fd168e30c98f4401ac59b5c6d9cc72
austintip <austin.d.tipograph@gmail.com>
2020-12-22 18:12:42 -0500

    made crawlers ogre moves with click

--- js/main.js
+++ js/main.js
@@ -9,14 +9,37 @@ let ctx = game.getContext('2d')
 game.setAttribute('height', getComputedStyle(game)['height'])
 game.setAttribute('width', getComputedStyle(game)['width'])
 
+function Crawler(x, y, width, height, color) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.width = width
+    this.height = height
+    this.alive = true
+    this.render = function() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}  
+
+let hero = new Crawler(50, 200, 50, 50, 'hotpink')
+let ogre = new Crawler(400, 150, 60, 100, '#bada55')
 //Draw a filled box
-let drawBox = (x, y, size, color) => {
-    ctx.fillStyle = color
-    ctx.fillRect(x, y, size, size)
-}
+// let drawBox = (x, y, size, color) => {
+//     ctx.fillStyle = color
+//     ctx.fillRect(x, y, size, size)
+// }
 
 game.addEventListener('click', (e) => {
-    drawBox(e.offsetX, e.offsetY, 100, 'hotpink')
-})
+    // drawBox(e.offsetX, e.offsetY, 100, 'hotpink')
+    //clear whole board
+    ctx.clearRect(0, 0, game.width, game.height)
+    hero.render()
+    // change ogre position
+    ogre.x = e.offsetX
+    ogre.y = e.offsetY
+    //draw ya boi
+    ogre.render()
+}, { option: true }) 
 
 
