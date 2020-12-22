8023fee7ba0abc117bced7026ad909346ccc8212
austintip <austin.d.tipograph@gmail.com>
2020-12-22 17:25:45 -0500

    user input boxes appear

--- js/main.js
+++ js/main.js
@@ -6,18 +6,17 @@ let statusDisplay = document.getElementById('status')
 
 // getting context for drawing on canvas
 let ctx = game.getContext('2d')
-// game.height = '400px'
-// game.width = '800px'
 game.setAttribute('height', getComputedStyle(game)['height'])
 game.setAttribute('width', getComputedStyle(game)['width'])
 
 //Draw a filled box
-//determines the fill color
-ctx.fillStyle = 'white'
-ctx.fillRect(10, 10, 100, 100)
+let drawBox = (x, y, size, color) => {
+    ctx.fillStyle = color
+    ctx.fillRect(x, y, size, size)
+}
 
-//Draw a line box
-// estab;ishing line looks
-ctx.strokeStyle = 'red'
-ctx.lineWidth = 10;
-ctx.strokeRect (10, 10, 100, 100)
+game.addEventListener('click', (e) => {
+    drawBox(e.offsetX, e.offsetY, 100, 'hotpink')
+})
+
+
