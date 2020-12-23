af9fee7ef426b9ba81eb1a9c2442059a5a47793c
austintip <austin.d.tipograph@gmail.com>
2020-12-22 19:31:48 -0500

    arrow keys move

--- js/main.js
+++ js/main.js
@@ -24,22 +24,69 @@ function Crawler(x, y, width, height, color) {
 
 let hero = new Crawler(50, 200, 50, 50, 'hotpink')
 let ogre = new Crawler(400, 150, 60, 100, '#bada55')
-//Draw a filled box
-// let drawBox = (x, y, size, color) => {
-//     ctx.fillStyle = color
-//     ctx.fillRect(x, y, size, size)
-// }
+let movement = 10
 
-game.addEventListener('click', (e) => {
-    // drawBox(e.offsetX, e.offsetY, 100, 'hotpink')
-    //clear whole board
+let gameLoop = () => {
+    //clear canvas every loop
     ctx.clearRect(0, 0, game.width, game.height)
+    //display gamestate on the DOM
+    movementDisplay.innerText = `X: ${hero.x}\nY: ${hero.y}`
+    // if ogre is alive
+    if (ogre.alive) {
+        //render the ogre
+        ogre.render()
+        //detect collision
+        detectHit()
+    }
+    //render the hero
     hero.render()
-    // change ogre position
-    ogre.x = e.offsetX
-    ogre.y = e.offsetY
-    //draw ya boi
-    ogre.render()
-}, { option: true }) 
+}
 
+let detectHit = () => {
+    //TODO: write collision detection
+    console.log('detecting hit')
+}
+
+let movementHandler = (e) => {
+    // move my hero based on the key pressed.
+    // if w is pressed
+    if (e.key === "ArrowUp"){ // move up
+        hero.y -= movement
+    } else if (e.key === 'ArrowLeft') { //move left
+        hero.x -= movement
+    } else if (e.key === 'ArrowDown') {//move down
+        hero.y += movement
+    } else if (e.key === 'ArrowRight') { // move right
+        hero.x += movement
+    } else {
+        console.log(`${e.key} won't make you move`)
+    }
+
+    /*
+    switch(e.key) {
+        case 'ArrowUp':
+            hero.y -= movement //moveup
+            break
+        case 'ArrowLeft': 
+            hero.x -= movement //move left
+            break
+        case 'ArrowDown':
+            hero.y += movement // move down
+            break
+        cast 'ArrowRight':
+            hero.x += movement // move right
+            break
+        default:
+            console.log(`${e.key} won't make you move`)
+    }*/
+}
+//set event listener for keydown
+document.addEventListener('keydown', movementHandler)
+//initializes the game
+let gameInterval = setInterval(gameLoop, 100)
+
+//helper function so my computer doesnt explode
+document.querySelector('#btm-left').addEventListener('click', () => {
+    clearInterval(gameInterval)
+})
 
